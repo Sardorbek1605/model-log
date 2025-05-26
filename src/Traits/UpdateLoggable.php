66a4905b163794ea6eaeba2f722eeee187bcda10
Sardorbek <abduqodirov.sardor1605@gmail.com>
2025-05-26 17:26:33 +0500

    Initial Commit

---
+++ src/Traits/UpdateLoggable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dostontiu\ModelLog\Traits;
+
+use Dostontiu\ModelLog\Services\ModelLogService;
+
+trait ModelUpdateLoggable
+{
+    public static function bootModelUpdateLoggable(): void
+    {
+        static::updating(function ($model) {
+            $dirty = $model->getDirty();
+            $original = $model->getOriginal();
+            new ModelLogService($model, $original, $dirty);
+        });
+    }
+}
